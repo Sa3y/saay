0567cf633ee083acbc81486af40e08cc82256210
Hana Alalwi <hanaalalwi1@gmail.com>
2017-08-09 02:09:39 +0300

    this file to connect to database

---
+++ configSa3i.php
@@ -0,0 +1,10 @@
+<?php
+$Host="127.0.0.1";
+$username="root";
+$password="";
+$DB="sa3i";
+$conn = mysqli_connect($Host,$username,$password,$DB);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
