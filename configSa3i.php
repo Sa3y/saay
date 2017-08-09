48825570d345e6627524bf956bbf68cdbcb43909
Khaled <alsahli.khaled.n@gmail.com>
2017-08-09 18:55:51 +0300

    database connection

--- configSa3i.php
+++ configSa3i.php
@@ -1,8 +1,8 @@
 <?php
-$Host="127.0.0.1";
-$username="root";
-$password="";
-$DB="sa3i";
+$Host="localhost";
+$username="id2515699_khalid";
+$password="q1q1q123";
+$DB="id2515699_sa3i";
 $conn = mysqli_connect($Host,$username,$password,$DB);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
