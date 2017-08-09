44e28ea0b04386b9617d88d37a2bb1b0c0ac4031
Khaled <alsahli.khaled.n@gmail.com>
2017-08-09 18:57:22 +0300

    database connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-if (! mysql_connect ( "localhost", "root", "" )) {
+if (! mysql_connect ( "localhost", "id2515699_khalid", "q1q1q123" )) {
 	die ( 'oops connection problem ! --> ' . mysql_error () );
 }
-if (! mysql_select_db ( "sa3i" )) {
+if (! mysql_select_db ( "id2515699_sa3i" )) {
 	die ( 'oops database selection problem ! --> ' . mysql_error () );
 }
 
