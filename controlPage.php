fdb56066acd1a72179bcd9b10aa8c9e7d5ef3f83
Hana Alalwi <hanaalalwi1@gmail.com>
2017-08-10 13:15:51 +0300

    edit the query

--- controlPage.php
+++ controlPage.php
@@ -86,64 +86,47 @@
                             
                             <div class="form-group">  
                                  <div class="col-lg-12 text-center">
-                            <div id="success"></div>
-                                     
-   <!-- Trigger/Open The Modal -->
-<button id="myBtn" type="submit" class="btn btn-xl" style="top: 100px; left: 335px;">قائمة أرقامي  </button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p><?php 
-        //to display the user's number from DB
+                            <div id="success"></div>                                     
+        <table border="0" width="400" style= "margin: 0 auto; " >
+      <thead>
+        <tr>
+          <th>أرقام الواتس اب </th>
         
-        $sql = "SELECT `Numbers` FROM `user` ";
+          <th>أرقام تيليجرام </th>
+          
+          <th>أرقام الرسائل النصية </th>  
+            </tr>
+      </thead>
+      <tbody>
+   <?php 
+        //to display the user's number from DB
+   
+                                   
+        $email=$_POST['email2'];
+                            if(isset($_POST['MyNumbers'])){                        
+                            
+        $sql = "SELECT `whatsApp`, `telegram`, `sms` FROM `numbers` WHERE  email='$email' ";
+      
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br>". $row["Numbers"]."<br>";
+        echo "<tr>"."<td>". $row["whatsApp"]."</td>".
+           
+            "<td>".$row["telegram"]."</td>"
+          
+            ."<td>".$row["sms"]."</td>"."</tr>";
+        
     }
 } else {
     echo "0 results";
-}?>  </p>
-  </div>
+}}?>                                  
+     
+                            
+            </tbody>
+                                     </table>
 
-</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-   
-                            </div>
+       </div>
                             </div>
                         </div>   
                         
