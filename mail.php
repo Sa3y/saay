f675e7a8a0c86093f216e617ff686ee33d21ac37
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2017-08-10 11:55:20 +0300

    Modified files to connect to db

--- mail.php
+++ mail.php
@@ -1,11 +1,19 @@
 <?php 
-  $from = "nwnlhrb@gmail.com";
+  session_start();
+  $db = mysqli_connect("localhost", "id2509065_sa3ymail", "sa3ymail", "id2509065_sa3y");
+  if(isset($_POST['submit'])) {
   $to=$_POST['email'];
-$subject="Welcome!";
-$body ="You've succesfully subscribed, thank you for your subscribtion, Please look forward for us to launch Sa3y!";
-    mail($to,$subject,$body); 
-	echo"شكرا لانضمامك إلى ساعٍ";
-	
-  include("index.html");
-  
+  $subject="Welcome!";
+  $body ="You've succesfully subscribed, thank you for your subscription, Please look forward for us to launch Sa3y!";
+  mail($to,$subject,$body);
+  $sql = "INSERT INTO mail(email) VALUES('$to')";
+  mysqli_query($db, $sql);
+  ?>
+  <script type="text/javascript">
+   alert("شكرا لك! لقد أصبحت ضمن أعضاء القائمة البريدية");
+   location="subConfirm.html";
+   </script>
+<?php
+  include("subConfirm.html");
+  }
 ?>
--- mail2.php
+++ mail2.php
@@ -1,11 +1,20 @@
 <?php 
-  $from = "nwnlhrb@gmail.com";
+  session_start();
+  $db = mysqli_connect("localhost", "id2509065_sa3ymail", "sa3ymail", "id2509065_sa3y");
+
+  if(isset($_POST['submit'])) {
   $to=$_POST['email'];
-$subject="Unsubscribed!";
-$body ="You've unsubscribed from Sa3y successfully! we hope to see again :)";
-    mail($to,$subject,$body); 
-	
-	include("unsubConfirm.html");
-	
-  
+  $subject="Unsubscribed!";
+  $body ="You've unsubscribed from Sa3y successfully! we hope to see again :)";
+  mail($to,$subject,$body);
+  $sql = "DELETE FROM mail WHERE email='$to'";
+  mysqli_query($db, $sql);
+?>
+  <script type="text/javascript">
+   alert(" تمت إزالتك من القائمة البريدية بنجاح!");
+   location="unsubConfirm.html";
+   </script>
+   <?php	
+  include("unsubConfirm.html");
+  }
 ?>
